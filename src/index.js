ab4a95158a88175c9f0ca9793901e79c05c0b377
Subhadip Chatterjee <50038188+Subhadip90@users.noreply.github.com>
2022-12-22 14:44:49 +0000

    new simple practice files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const fname = "Subhadip";
+const lname = "Chatterjee";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <>
+    <h1 style={{ color: "blue" }}>{`My name is ${fname} ${lname}`}</h1>
+  </>, document.getElementById("root"));
